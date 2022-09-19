b4401d0be156cd7b2687075cac5156986e916e72
stefankrajisnik <stefankrajisnik95@gmail.com>
2022-09-19 21:41:40 +0200

    slides images and function added

--- script.js
+++ script.js
@@ -17,3 +17,62 @@ const topSection = document.querySelector('.top-section')
 downArrow.addEventListener('click', ()=> {
     topSection.style.transform = 'translateY(-100vh)'
 });
+
+
+const btnLeft = document.querySelector('.fa-angle-left');
+const btnRight = document.querySelector('.fa-angle-right');
+let slides = document.querySelectorAll('.single-slide');
+let index = 0;
+let currentSlide = slides[index];
+
+function setCurrentSlide() {
+    currentSlide = slides[index];
+}
+
+function showNextSlide() {
+    currentSlide.classList.remove('show');
+    index++;
+    setCurrentSlide()
+    currentSlide.classList.add('show');
+}
+
+function showPreviousSlide() {
+    currentSlide.classList.remove('show');
+    index--;
+    setCurrentSlide()
+    currentSlide.classList.add('show');
+}
+
+function startFromZero() {
+    currentSlide.classList.remove('show');
+    index = 0;
+    setCurrentSlide()
+    currentSlide.classList.add('show');
+}
+
+function startFromEnd() {
+    currentSlide.classList.remove('show');
+    index = slides.length - 1;
+    setCurrentSlide()
+    currentSlide.classList.add('show');
+}
+
+btnRight.addEventListener('click', () => {
+    
+    if(index !== slides.length - 1){
+        console.log(`index is not ZERO, index was ${index}`)
+
+        showNextSlide()
+    }else {
+        startFromZero()
+    }
+})
+
+btnLeft.addEventListener('click', () => {
+   
+    if(index !== 0) {
+        showPreviousSlide()
+    } else {
+        startFromEnd()
+    }
+})
